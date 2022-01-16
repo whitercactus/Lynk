0ca0ec9b0ca0efd98555a7b0ada03a29b55f4fcb
whitercactus <whitercactus927@gmail.com>
2022-01-16 15:16:20 -0800

    boilerplate stuff

---
+++ build/CMakeFiles/Lynk.dir/src/main.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/Lynk.dir/src/main.cpp.o: /home/nick/Code/Lynk/src/main.cpp \
+ /usr/include/stdc-predef.h \
+ /home/nick/Code/Lynk/build/_deps/raylib-src/src/raylib.h \
+ /usr/lib/gcc/aarch64-redhat-linux/11/include/stdarg.h
---
+++ build/_deps/raylib-build/raylib/CMakeFiles/raylib.dir/rtextures.c.o.d
@@ -0,0 +1,56 @@
+_deps/raylib-build/raylib/CMakeFiles/raylib.dir/rtextures.c.o: \
+ /home/nick/Code/Lynk/build/_deps/raylib-src/src/rtextures.c \
+ /usr/include/stdc-predef.h \
+ /home/nick/Code/Lynk/build/_deps/raylib-src/src/raylib.h \
+ /usr/lib/gcc/aarch64-redhat-linux/11/include/stdarg.h \
+ /usr/lib/gcc/aarch64-redhat-linux/11/include/stdbool.h \
+ /home/nick/Code/Lynk/build/_deps/raylib-src/src/config.h \
+ /home/nick/Code/Lynk/build/_deps/raylib-src/src/utils.h \
+ /home/nick/Code/Lynk/build/_deps/raylib-src/src/rlgl.h \
+ /usr/include/stdlib.h /usr/include/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/bits/wordsize.h /usr/include/bits/timesize.h \
+ /usr/include/sys/cdefs.h /usr/include/bits/long-double.h \
+ /usr/include/gnu/stubs.h /usr/include/gnu/stubs-lp64.h \
+ /usr/lib/gcc/aarch64-redhat-linux/11/include/stddef.h \
+ /usr/include/bits/waitflags.h /usr/include/bits/waitstatus.h \
+ /usr/include/bits/floatn.h /usr/include/bits/floatn-common.h \
+ /usr/include/sys/types.h /usr/include/bits/types.h \
+ /usr/include/bits/typesizes.h /usr/include/bits/time64.h \
+ /usr/include/bits/types/clock_t.h /usr/include/bits/types/clockid_t.h \
+ /usr/include/bits/types/time_t.h /usr/include/bits/types/timer_t.h \
+ /usr/include/bits/stdint-intn.h /usr/include/endian.h \
+ /usr/include/bits/endian.h /usr/include/bits/endianness.h \
+ /usr/include/bits/byteswap.h /usr/include/bits/uintn-identity.h \
+ /usr/include/sys/select.h /usr/include/bits/select.h \
+ /usr/include/bits/types/sigset_t.h /usr/include/bits/types/__sigset_t.h \
+ /usr/include/bits/types/struct_timeval.h \
+ /usr/include/bits/types/struct_timespec.h \
+ /usr/include/bits/pthreadtypes.h /usr/include/bits/thread-shared-types.h \
+ /usr/include/bits/pthreadtypes-arch.h /usr/include/bits/struct_mutex.h \
+ /usr/include/bits/struct_rwlock.h /usr/include/alloca.h \
+ /usr/include/bits/stdlib-float.h /usr/include/string.h \
+ /usr/include/bits/types/locale_t.h /usr/include/bits/types/__locale_t.h \
+ /usr/include/strings.h /usr/include/math.h \
+ /usr/include/bits/math-vector.h /usr/include/bits/libm-simd-decl-stubs.h \
+ /usr/include/bits/flt-eval-method.h /usr/include/bits/fp-logb.h \
+ /usr/include/bits/fp-fast.h \
+ /usr/include/bits/mathcalls-helper-functions.h \
+ /usr/include/bits/mathcalls.h /usr/include/stdio.h \
+ /usr/include/bits/types/__fpos_t.h /usr/include/bits/types/__mbstate_t.h \
+ /usr/include/bits/types/__fpos64_t.h /usr/include/bits/types/__FILE.h \
+ /usr/include/bits/types/FILE.h /usr/include/bits/types/struct_FILE.h \
+ /usr/include/bits/stdio_lim.h \
+ /home/nick/Code/Lynk/build/_deps/raylib-src/src/external/stb_image.h \
+ /usr/lib/gcc/aarch64-redhat-linux/11/include/limits.h \
+ /usr/lib/gcc/aarch64-redhat-linux/11/include/syslimits.h \
+ /usr/include/limits.h /usr/include/bits/posix1_lim.h \
+ /usr/include/bits/local_lim.h /usr/include/linux/limits.h \
+ /usr/include/bits/pthread_stack_min-dynamic.h \
+ /usr/include/bits/pthread_stack_min.h /usr/include/bits/posix2_lim.h \
+ /usr/include/assert.h \
+ /usr/lib/gcc/aarch64-redhat-linux/11/include/stdint.h \
+ /usr/include/stdint.h /usr/include/bits/wchar.h \
+ /usr/include/bits/stdint-uintn.h \
+ /home/nick/Code/Lynk/build/_deps/raylib-src/src/external/stb_image_write.h \
+ /home/nick/Code/Lynk/build/_deps/raylib-src/src/external/stb_image_resize.h

45c85b772708abf9cf807b95f7f520630dcae310
TrashCan <61761890+whitercactus@users.noreply.github.com>
2022-01-16 15:18:36 -0800

    Add files via upload
    boilerplate stuff

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,17 @@
 
 int main(int argc, char **argv)
 {
+    InitWindow(680, 450, "Lynk");
+    SetTargetFPS(60);
+
+    while(!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(BLACK);
+        DrawCircleV({340, 225}, 40, WHITE);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
     return 0;
 }
